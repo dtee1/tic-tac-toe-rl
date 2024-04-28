001358df060fe1cd16e10cc4e538a46898849309
dtee1 <davidtalson0@gmail.com>
2024-04-28 10:11:08 -0400

    Refactor learning to use deep q network

--- agent.js
+++ agent.js
@@ -1,234 +1,238 @@
 class TicTacToeAgent {
-  constructor(
-    boardSize = 3,
-    learningRate = 0.001,
-    gamma = 0.9,
-    epsilonDecay = 0.995,
-  ) {
-    this.boardSize = boardSize;
-    this.learningRate = learningRate;
-    this.gamma = gamma;
-    this.epsilonDecay = epsilonDecay;
-    this.epsilon = 1.0;
-    this.qTable = {};
-    this.memory = [];
-    this.batchSize = 32;
-    this.winCounts = { X: 0, O: 0, Draw: 0 };
-    this.highestWinningPercentage = 0;
-    this.bestAgentData = null;
+  constructor() {
+    this.board = [
+      ['-', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-'],
+    ];
+    this.players = ['X', 'O'];
+    this.num_players = this.players.length;
+    this.Q = {};
+    this.learningRate = 0.001;
+    this.discountFactor = 0.5;
+    this.explorationRate = 0.25;
+    this.numEpisodes = 1000;
   }
 
-  convertToInputTensor(gameState) {
-    const flatState = gameState.flat();
-    return flatState;
-  }
-
-  storeExperience(state, action, reward, nextState) {
-    this.memory.push({ state, action, reward, nextState });
-  }
-
-  updateWinCounts(outcome) {
-    if (outcome === 'X wins!') {
-      this.winCounts.X++;
-    } else if (outcome === 'O wins!') {
-      this.winCounts.O++;
-    } else {
-      this.winCounts.Draw++;
-    }
-
-    const totalGames =
-      this.winCounts.X + this.winCounts.O + this.winCounts.Draw;
-    const winPercentage = (this.winCounts.X / totalGames) * 100;
-    if (winPercentage > this.highestWinningPercentage) {
-      this.highestWinningPercentage = winPercentage;
-      this.bestAgentData = {
-        boardSize: this.boardSize,
-        learningRate: this.learningRate,
-        gamma: this.gamma,
-        epsilonDecay: this.epsilonDecay,
-        epsilon: this.epsilon,
-        memory: this.memory,
-        batchSize: this.batchSize,
-        winCounts: { ...this.winCounts },
-      };
-    }
-  }
-
-  async train(episodes) {
-    for (let episode = 0; episode < episodes; episode++) {
-      let gameState = Array(this.boardSize * this.boardSize).fill('-');
-      let gameActive = true;
-
-      while (gameActive) {
-        const actionX = await this.makeMove(gameState);
-        gameState[actionX] = 'X';
-        if (this.checkWin(gameState, 'X')) {
-          this.storeExperience(
-            gameState,
-            actionX,
-            this.assignReward('X wins!'),
-            Array(this.boardSize * this.boardSize).fill('-'),
-          );
-          this.updateWinCounts('X wins!');
-          gameActive = false;
-          break;
-        } else if (this.checkDraw(gameState)) {
-          this.storeExperience(
-            gameState,
-            actionX,
-            this.assignReward("It's a draw!"),
-            Array(this.boardSize * this.boardSize).fill('-'),
-          );
-          this.updateWinCounts("It's a draw!");
-          gameActive = false;
-          break;
-        }
-
-        const actionO = await this.makeMove(gameState);
-        gameState[actionO] = 'O';
-        if (this.checkWin(gameState, 'O')) {
-          this.storeExperience(
-            gameState,
-            actionO,
-            this.assignReward('O wins!'),
-            Array(this.boardSize * this.boardSize).fill('-'),
-          );
-          this.updateWinCounts('O wins!');
-          gameActive = false;
-          break;
-        } else if (this.checkDraw(gameState)) {
-          this.storeExperience(
-            gameState,
-            actionO,
-            this.assignReward("It's a draw!"),
-            Array(this.boardSize * this.boardSize).fill('-'),
-          );
-          this.updateWinCounts("It's a draw!");
-          gameActive = false;
-          break;
-        }
-      }
-    }
-
-    this.saveBestAgent();
-  }
-
-  // makeMove(gameState) {
-  //   if (Math.random() < this.epsilon) {
-  //     const availableMoves = gameState
-  //       .map((cell, index) => (cell === '-' ? index : -1))
-  //       .filter((index) => index !== -1);
-  //     return availableMoves[Math.floor(Math.random() * availableMoves.length)];
-  //   } else {
-  //     // Use Q-table to make a move
-  //     const stateIndex = this.getStateIndex(gameState);
-  //     const qValues = this.qTable[stateIndex];
-  //     const availableMoves = gameState
-  //       .map((cell, index) => (cell === '-' ? index : -1))
-  //       .filter((index) => index !== -1);
-  //     const legalMovesQValues = availableMoves.map((move) => qValues[move]);
-  //     const bestMoveIndex =
-  //       availableMoves[
-  //         legalMovesQValues.indexOf(Math.max(...legalMovesQValues))
-  //       ];
-  //     // console.log(bestMoveIndex);
-  //     return bestMoveIndex;
+  // printBoard() {
+  //   for (let row of this.board) {
+  //     console.log(row.join('  |  '));
+  //     console.log('---------------');
   //   }
   // }
-  makeMove(gameState) {
-    // Function to check if a move blocks the opponent from winning
-    const blockOpponentWinningMove = (gameState, index) => {
-      const opponent = this.currentPlayer === 'X' ? 'O' : 'X';
-      const nextState = [...gameState];
-      nextState[index] = opponent;
-      return this.checkWin(nextState, opponent);
-    };
 
-    // Function to check if a move leads to winning
-    const winningMove = (gameState, index) => {
-      const nextState = [...gameState];
-      nextState[index] = this.currentPlayer;
-      return this.checkWin(nextState, this.currentPlayer);
-    };
+  boardToString() {
+    return this.board.flat().join('');
+  }
 
-    // Prioritize winning moves
-    const winningMoves = gameState
-      .map((cell, index) => (cell === '-' && winningMove(gameState, index) ? index : -1))
-      .filter(index => index !== -1);
-
-    if (winningMoves.length > 0) {
-      // Make a winning move if available
-      return winningMoves[0];
-    }
-
-    // If no winning move, prioritize blocking the opponent from winning
-    const opponentWinningMoves = gameState
-      .map((cell, index) => (cell === '-' && blockOpponentWinningMove(gameState, index) ? index : -1))
-      .filter(index => index !== -1);
-
-    if (opponentWinningMoves.length > 0) {
-      // Block the opponent from winning
-      return opponentWinningMoves[0];
-    } else {
-      // If no immediate threat or winning move, proceed with the default strategy
-      if (Math.random() < this.epsilon) {
-        // Random move with epsilon-greedy policy
-        const availableMoves = gameState
-          .map((cell, index) => (cell === '-' ? index : -1))
-          .filter(index => index !== -1);
-        return availableMoves[Math.floor(Math.random() * availableMoves.length)];
-      } else {
-        // Use Q-table to make a move
-        const stateIndex = this.getStateIndex(gameState);
-        const qValues = this.qTable[stateIndex];
-        const availableMoves = gameState
-          .map((cell, index) => (cell === '-' ? index : -1))
-          .filter(index => index !== -1);
-        const legalMovesQValues = availableMoves.map(move => qValues[move]);
-        const bestMoveIndex = availableMoves[legalMovesQValues.indexOf(Math.max(...legalMovesQValues))];
-        return bestMoveIndex;
+  isGameOver() {
+    for (let row of this.board) {
+      if (new Set(row).size === 1 && row[0] !== '-') {
+        return [true, row[0]];
       }
     }
+
+    const transpose = this.board[0].map((_, colIndex) =>
+      this.board.map((row) => row[colIndex]),
+    );
+    for (let col of transpose) {
+      if (new Set(col).size === 1 && col[0] !== '-') {
+        return [true, col[0]];
+      }
+    }
+
+    const mainDiagonal = this.board.map((row, index) => row[index]);
+    const antiDiagonal = this.board.map(
+      (row, index) => row[row.length - index - 1],
+    );
+    if (new Set(mainDiagonal).size === 1 && mainDiagonal[0] !== '-') {
+      return [true, mainDiagonal[0]];
+    }
+    if (new Set(antiDiagonal).size === 1 && antiDiagonal[0] !== '-') {
+      return [true, antiDiagonal[0]];
+    }
+
+    if (!this.board.flat().includes('-')) {
+      return [true, 'draw'];
+    }
+
+    return [false, ''];
   }
 
-
-  assignReward(outcome) {
-    if (outcome === 'X wins!') {
-      return 1;
-    } else if (outcome === 'O wins!') {
-      return -1;
+  chooseAction() {
+    const state = this.boardToString();
+    console.log(this.Q[state]);
+    if (Math.random() < this.explorationRate || !(state in this.Q)) {
+      const emptyCells = this.board.reduce((acc, row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+          if (cell === '-') acc.push([rowIndex, colIndex]);
+        });
+        return acc;
+      }, []);
+      return emptyCells[Math.floor(Math.random() * emptyCells.length)];
     } else {
-      return 0;
+      const qValues = this.Q[state];
+      const emptyCells = this.board.reduce((acc, row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+          if (cell === '-') acc.push([rowIndex, colIndex]);
+        });
+        return acc;
+      }, []);
+      const emptyQValues = emptyCells.map((cell) => qValues[cell[0]][cell[1]]);
+      const maxQValue = Math.max(...emptyQValues);
+      const maxQIndices = emptyQValues.reduce((acc, val, index) => {
+        if (val === maxQValue) acc.push(index);
+        return acc;
+      }, []);
+      const maxQIndex =
+        maxQIndices[Math.floor(Math.random() * maxQIndices.length)];
+      return emptyCells[maxQIndex];
     }
   }
 
-  checkWin(gameState, player) {
-    const winningCombinations = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical columns
-      [0, 4, 8], [2, 4, 6] // Diagonals
-    ];
+  boardNextState(cell) {
+    const nextBoard = this.board.map((row) => [...row]);
+    nextBoard[cell[0]][cell[1]] = this.players[0];
+    return nextBoard;
+  }
 
+  updateQTable(state, action, nextState, reward) {
+    const qValues =
+      this.Q[state] || Array.from({ length: 3 }, () => Array(3).fill(0));
 
-    return winningCombinations.some((combination) => {
-      const [a, b, c] = combination;
-      if (
-        gameState[a] === player &&
-        gameState[b] === player &&
-        gameState[c] === player
-      ) {
-        // console.log(`Player ${player} wins with combination: ${combination}`);
-        return true;
+    const nextQValues =
+      this.Q[this.boardToString(nextState)] ||
+      Array.from({ length: 3 }, () => Array(3).fill(0));
+    const maxNextQValue = Math.max(...nextQValues.flat());
+
+    qValues[action[0]][action[1]] +=
+      this.learningRate *
+      (reward +
+        this.discountFactor * maxNextQValue -
+        qValues[action[0]][action[1]]);
+    console.log(this.boardToString(nextState));
+    this.Q[state] = qValues;
+  }
+
+  startTrain(episodes, learningRate) {
+    this.numEpisodes = episodes;
+    this.learningRate = learningRate;
+    for (let episode = 0; episode < this.numEpisodes; episode++) {
+      this.board = Array.from({ length: 3 }, () => Array(3).fill('-'));
+
+      let current_player =
+        this.players[Math.floor(Math.random() * this.num_players)];
+      let isGameOverState = false;
+      let winner;
+
+      while (!isGameOverState) {
+        const action = this.chooseAction();
+
+        if (action) {
+          const [row, col] = action;
+          this.board[row][col] = current_player;
+        } else {
+          console.error('Action is undefined');
+          break;
+        }
+
+        [isGameOverState, winner] = this.isGameOver();
+
+        if (isGameOverState) {
+          let reward;
+          if (winner === current_player) reward = 1;
+          else if (winner === 'draw') reward = 0.5;
+          else reward = 0;
+          this.updateQTable(this.boardToString(), action, this.board, reward);
+        } else {
+          current_player =
+            this.players[
+              (this.players.indexOf(current_player) + 1) % this.num_players
+            ];
+        }
+
+        if (!isGameOverState) {
+          const nextState = this.boardNextState(action);
+          this.updateQTable(this.boardToString(), action, nextState, 0);
+        }
       }
-    });
-  }
 
-  checkDraw(gameState) {
-    return gameState.every((cell) => cell !== '-');
+      this.explorationRate *= 0.99;
+      console.log('Here');
+    }
+    localStorage.setItem('QState', JSON.stringify(this.Q));
+    return true;
   }
+  // playVsHuman() {
+  //   let current_player =
+  //     this.players[Math.floor(Math.random() * this.num_players)];
+  //   let isGameOverState = false;
+  //   let winner;
+  //   this.board = [
+  //     ['-', '-', '-'],
+  //     ['-', '-', '-'],
+  //     ['-', '-', '-'],
+  //   ];
 
-  async saveBestAgent() {
-    localStorage.setItem('bestAgentData', JSON.stringify(this.bestAgentData));
-    console.log('Best agent saved.');
+  //   while (!isGameOverState) {
+  //     // Agent's turn
+  //     if (current_player === 'X') {
+  //       const action = this.chooseAction();
+  //       const [row, col] = action;
+  //       this.board[row][col] = current_player;
+  //     } else {
+  //       // Human's turn
+  //       this.printBoard();
+  //       let row = parseInt(prompt('Enter the row (0-2): '));
+  //       let col = parseInt(prompt('Enter the column (0-2): '));
+  //       while (
+  //         isNaN(row) ||
+  //         isNaN(col) ||
+  //         row < 0 ||
+  //         row > 2 ||
+  //         col < 0 ||
+  //         col > 2 ||
+  //         this.board[row][col] !== '-'
+  //       ) {
+  //         console.log('Invalid input. Please enter again.');
+  //         row = parseInt(prompt('Enter the row (0-2): '));
+  //         col = parseInt(prompt('Enter the column (0-2): '));
+  //       }
+  //       this.board[row][col] = current_player;
+  //     }
+
+  //     // Check if game is over
+  //     [isGameOverState, winner] = this.isGameOver();
+  //     if (isGameOverState) {
+  //       this.printBoard();
+  //       if (winner === 'X') {
+  //         console.log('Agent wins!');
+  //       } else if (winner === 'O') {
+  //         console.log('Human wins!');
+  //       } else {
+  //         console.log("It's a draw!");
+  //       }
+  //     } else {
+  //       // Switch player
+  //       current_player =
+  //         this.players[
+  //           (this.players.indexOf(current_player) + 1) % this.num_players
+  //         ];
+  //     }
+  //   }
+  // }
+
+  loadQState() {
+    // Load the Q-state from memory (e.g., localStorage)
+    const savedQState = localStorage.getItem('QState');
+    if (savedQState) {
+      this.Q = JSON.parse(savedQState);
+    }
+  }
+  agentTurn(board) {
+    this.board = board;
+    const action = this.chooseAction();
+
+    return action;
   }
 }
